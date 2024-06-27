f216e0d74d9852dfdee912af3e3444eecad547f9
MahetsiEdahi <daniel.olvera.v1905@gmail.com>
2024-06-27 11:08:09 -0600

    Code && Version Update2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.4"
 REPO_NAME = "DataBase_Connection"
-PKG_NAME= "databaseautomation"
+PKG_NAME= "DBConnection"
 AUTHOR_USER_NAME = "MahetsiEdhai"
 AUTHOR_EMAIL = "daniel.olvera.v1905@gmail.com"
 
